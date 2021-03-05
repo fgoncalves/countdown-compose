dfb8e9b3f9ccbbd0629c1d959e8a4b8d71488590
fred <frederico.goncalves@soundcloud.com>
2021-03-05 18:13:00 +0100

    Manage to fix upper bound bug

--- app/src/main/java/com/example/androiddevchallenge/ui/utils/TimeUtils.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/utils/TimeUtils.kt
@@ -2,13 +2,13 @@ package com.example.androiddevchallenge.ui.utils
 
 fun Int.asTimeString(): String =
     "%02dh %02dm %02ds".format(
-        this / 10000 % 100,
+        this / 10000,
         this / 100 % 100,
         this % 100,
     )
 
 fun Int.asMillis(): Int {
-    val hours = this / 10000 % 100
+    val hours = this / 10000
     val minutes = this / 100 % 100
     val seconds = this % 100
 
